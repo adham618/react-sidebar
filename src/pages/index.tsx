c5c50ded15e694e9fa0e6ca87f1225b2dc2d721b
adham618 <adhamtarek291@gmail.com>
2022-06-29 00:44:53 +0200

    add sidebar component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
+import Sidebar from '@/components/Sidebar';
 
 export default function HomePage() {
   return (
     <Layout>
       <Seo />
+      <Sidebar />
       <main>
         <section className='bg-white'>
           <div className='layout flex min-h-screen flex-col items-center justify-center text-center'></div>
